bf3fefe30915a2c6ec62ab691dca121fed70def9
Alexander Lyabah <a.lyabah@checkio.org>
2019-02-25 16:08:05 +0200

    templates

---
+++ bears/website/website/views.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.views.generic.edit import FormView
+from fastai import load_learner
+from django.conf import settings
+from django.http import HttpResponse
+
+learn = load_learner(settings.LEARN_FOLDER)
+
+class ExaminatePicture(forms.Form):
+    pic = forms.ImageField()
+
+class ExaminateView(FormView):
+    form_class = ExaminatePicture
+    template_name = 'index.html'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        
+        pred_class,_,losses = learner.predict(form.files['pic'].read())
+        return HttpResponse(str(pred_class))
+        
