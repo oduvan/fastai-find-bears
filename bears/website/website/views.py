93528737d33a7b0dd41a1ae03c23d1a3b675107a
Alexander Lyabah <a.lyabah@checkio.org>
2019-02-25 19:42:37 +0200

    code styling

--- bears/website/website/views.py
+++ bears/website/website/views.py
@@ -1,13 +1,17 @@
+from io import BytesIO
+
 from django import forms
 from django.views.generic.edit import FormView
-from fastai.basic_train import load_learner
-from fastai.vision.image import open_image
 from django.conf import settings
 from django.http import HttpResponse
-from io import BytesIO
 from django.shortcuts import render
 
-learn = load_learner(settings.LEARN_FOLDER)
+from fastai.basic_train import load_learner
+from fastai.vision.image import open_image
+
+
+LEARN = load_learner(settings.LEARN_FOLDER)
+
 
 class ExaminatePicture(forms.Form):
     pic = forms.ImageField()
@@ -17,11 +21,9 @@ class ExaminateView(FormView):
     template_name = 'index.html'
 
     def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
         
-        _,_,outputs = learn.predict(open_image(BytesIO(form.files['pic'].read())))
-        predictions = zip(learn.data.classes, outputs)
+        _,_,outputs = LEARN.predict(open_image(BytesIO(form.files['pic'].read())))
+        predictions = zip(LEARN.data.classes, outputs)
 
         return render(self.request, self.template_name, {
             'form': form,
